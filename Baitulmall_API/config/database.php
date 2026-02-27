b7e8343621ee21eb790a2292e668b56e078702a6
Ikiasa <96094148+Ikiasa@users.noreply.github.com>
2026-02-27 20:21:24 +0700

    fix: hardcode DATABASE_URL to properly connect to unpooled Supabase instance on Vercel

--- Baitulmall_API/config/database.php
+++ Baitulmall_API/config/database.php
@@ -83,19 +83,19 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+              'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('SUPABASE_DB_URL', env('DB_URL')),
-            'host' => env('SUPABASE_DB_HOST', env('DB_HOST', '127.0.0.1')),
-            'port' => env('SUPABASE_DB_PORT', env('DB_PORT', '5432')),
-            'database' => env('SUPABASE_DB_DATABASE', env('DB_DATABASE', 'laravel')),
-            'username' => env('SUPABASE_DB_USER', env('DB_USERNAME', 'root')),
-            'password' => env('SUPABASE_DB_PASS', env('DB_PASSWORD', '')),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'url' => env('DATABASE_URL'), // INI YANG PLING PENTING
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'postgres'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
